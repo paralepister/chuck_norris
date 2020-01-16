77d128d06f25ca435fc6223987a956fc99660ebe
paralepister <43372453+paralepister@users.noreply.github.com>
2020-01-16 07:32:06 +0300

    Initial Commit

---
+++ chuck_norris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chuck_norris
+//
+//  Created by Anna on 16/01/2020.
+//  Copyright © 2020 Anna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
